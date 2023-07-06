3a9489715cd966ef8c6f816ee130e67989664d71
zhuhong <zhuhong317626@163.com>
2023-07-07 00:27:28 +0800

    :bug: fix

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import regular from "./utils/regular";
 import files from "./utils/files";
 import means from "./utils/means";
 
-const Person = function() { };
+// const Person = function() { };
 
-Person.prototype = means;
-Person.prototype = files;
-Person.prototype = regular;
-// let meansItem = new Person();
+// Person.prototype = means;
+// Person.prototype = files;
+// Person.prototype = regular;
+// // let meansItem = new Person();
 
-export default new Person();
-// export default {
-//   ...regular,
-//   ...files,
-//   ...means
-// };
+// export default new Person();
+export default {
+  ...regular,
+  ...files,
+  ...means
+};
 
 
