9d281a1e8ad1d1cb923c4671e6efa910d5280db8
zhuhong <zhuhong317626@163.com>
2023-07-08 11:50:28 +0800

    :bug: fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,15 @@
-declare const _default: any;
+declare const _default: {
+    urlToObjet(url: string): Object;
+    ascii(obj?: Object): any;
+    trim(str: string, type?: string | number): string;
+    randomColor(): string;
+    max(arr: any[], key?: string | undefined): string | number | void;
+    min(arr?: any[], key?: string | undefined): string | number | void;
+    throttle(fn: Function, time?: number): void;
+    shake(fn: Function, time?: number): void;
+    getImageInfo(imgUrl: String): Promise<unknown>;
+    base64toFile(base: String, filename?: any): File;
+    phone(phone: string): Boolean;
+    identityCard(card: string): Boolean;
+};
 export default _default;
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,9 @@ import regular from "./utils/regular";
 import files from "./utils/files";
 import means from "./utils/means";
 
-// const Person = function() { };
-
-// Person.prototype = means;
-// Person.prototype = files;
-// Person.prototype = regular;
-// // let meansItem = new Person();
-
-// export default new Person();
 export default {
   ...regular,
   ...files,
   ...means
 };
-
 
